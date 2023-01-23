c38ef9b6cfea88cbb46d9b665439b82c69b2b7aa
heindess <122467871+heindess@users.noreply.github.com>
2023-01-23 18:49:20 +0300

    Add files via upload

---
+++ hw2.js
@@ -0,0 +1,66 @@
+//  let x = 20;
+//  let y = 58;
+//  let z = 42;
+//  alert(z + x + y);
+
+// // alert(60 * 60);
+// // alert(60 * 60 * 24);
+// // alert(60 * 24);
+// // alert(24 * 360);
+
+// var today = new Date(2023-01-23);
+// var birthday = new Date("2004-10-21")
+// var difference = today - birthday;
+// alert(Math.ceil(difference / (1000 * 3600 * 24)));
+
+// var a = 5; //число
+// var b = "5"; //строка
+// var c = a + b; 
+// alert(c);//выведет 55
+// alert(typeof(c))// выведет string
+
+// let aa = 1;
+// let bb = 2;
+// let cc = "белых медведей";
+
+// console.log(String(aa) + bb + " " + cc);
+
+// // let доступ;
+// // let морпех;
+// // let наледь;
+// // let попрек;
+// // let рубило;
+// // let lengthWords;
+
+// let text = 'рубило,попрек,наледь,морпех,доступ';
+ 
+// function getWordLengths(str) {
+//     let len = [];
+//     let words = str.split(' ');
+//     for (let i = 0; i < words.length; i++) {
+//         len.push(words[i].length);
+//     }
+//     return len;
+// }
+// console.log(getWordLengths(text));
+
+// let num4 = 1;
+// const a = "Variable: num4 have type: "+ typeof num4
+// console.log(a)
+
+// let num5 = true;
+// const a = "Variable: num4 have type: "+ typeof num5
+// console.log(a)
+
+// const num6 = "andrey";
+// const a = "Variable: num4 have type: "+ typeof num6
+// console.log(a)
+
+// `var name = prompt ("NAME");
+//          var age = prompt('age?',18);
+         
+//          alert ('Hello' + name);
+//          var message = (age < 18) ? 'vi minor':
+//          alert(message);
+
+
